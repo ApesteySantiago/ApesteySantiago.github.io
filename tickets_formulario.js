1034550431e7dd830f1eef7cc3f0e9ebcbc5f4ea
ApesteySantiago <apesteysantiago@hotmail.com>
2023-06-11 11:52:20 -0300

    Adhesion de Integrador_JavaScript - parte 2

--- tickets_formulario.js
+++ tickets_formulario.js
@@ -1,18 +1,20 @@
-// Obtener los elementos del DOM
-formulario = document.getElementById('formulario');
-cantidadInput = document.getElementById('cantidad');
-categoriaSelect = document.getElementById('categoria');
-totalMsg = document.getElementById('total-msg');
-borrarBtn = document.getElementById('borrar-btn');
-resumenBtn = document.getElementById('resumen-btn');
+document.addEventListener('DOMContentLoaded', function() {
 
-// Asignar evento de clic al botón de Borrar
-borrarBtn.addEventListener('click', function() {
+  // Obtener los elementos del DOM
+  formulario = document.getElementById('formulario');
+  cantidadInput = document.getElementById('cantidad');
+  categoriaSelect = document.getElementById('categoria');
+  totalMsg = document.getElementById('total-msg');
+  borrarBtn = document.getElementById('borrar-btn');
+  resumenBtn = document.getElementById('resumen-btn');
+  
+  // Asignar evento de clic al botón de Borrar
+  borrarBtn.addEventListener('click', function() {
   formulario.reset();
-});
+  });
 
-// Asignar evento de clic al botón de Resumen
-resumenBtn.addEventListener('click', function() {
+  // Asignar evento de clic al botón de Resumen
+  resumenBtn.addEventListener('click', function() {
   cantidad = parseInt(cantidadInput.value);
   categoria = categoriaSelect.value;
   let descuento = 0;
@@ -33,4 +35,7 @@ resumenBtn.addEventListener('click', function() {
 
   const total = 200 * cantidad * (1 - descuento).toFixed(2);
   totalMsg.textContent = `Total a pagar: $${total}`;
+  });
+
+
 });
